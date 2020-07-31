066b673ce1399954e86be23605fed42064eea279
Thomas ROUSSEAU <thomas.rousseau@protonmail.com>
2020-07-31 23:51:15 +0200

    Mise en place du projet, route Home

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class HomeController
+{
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/", name="Home")
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function index(): Response
+    {
+        return new Response($this->twig->render('pages/home.html.twig'));
+    }
+}
