eec2de6e89c6fc60fa8d07322f40c63aad0e24aa
Thomas ROUSSEAU <thomas.rousseau@protonmail.com>
2020-08-08 00:40:23 +0200

    Construction du formulaire

--- src/Forms/ArretForm.php
+++ src/Forms/ArretForm.php
@@ -4,8 +4,8 @@ namespace App\Forms;
 
 use App\Entity\Arret;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +14,25 @@ class ArretForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nomArret', TextType::class, [
+            ->add('nomArret', ChoiceType::class, [
+                'choices' => [
+                    'Choisissez une station' => '-1',
+                    'J.F. Kennedy' => 'J.F. Kennedy',
+                    'Italie' => 'Italie',
+                    'Anatole France' => 'Anatole France',
+                    'Charles de Gaulle' => 'Charles de Gaulle',
+                    'Clemenceau' => 'Clemenceau',
+                    'Gares' => 'Gares',
+                    'Henri Fréville' => 'Henri Fréville',
+                    'Jacques Cartier' => 'Jacques Cartier',
+                    'La Poterie' => 'La Poterie',
+                    'Le Blosne' => 'Le Blosne',
+                    'Pontchaillou' => 'Pontchaillou',
+                    'République' => 'République',
+                    'Sainte-Anne' => 'Sainte-Anne',
+                    'Triangle' => 'Triangle',
+                    'Villejean-Université' => 'Villejean-Université'
+                ],
                 'required' => true,
                 'label' => false,
                 'attr' => [
