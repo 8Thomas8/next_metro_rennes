720732739e93adc8c4930af58696afe691e190f5
Thomas ROUSSEAU <thomas.rousseau@protonmail.com>
2020-08-07 21:13:26 +0200

    Réalisation header

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,9 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+import '../scss/global.scss';
+import '../scss/pages/base.scss';
+import '../scss/components/header.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
