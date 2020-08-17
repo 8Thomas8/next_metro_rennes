27dd744250f1e52325e8eb342582568d66053b21
Thomas ROUSSEAU <thomas.rousseau@protonmail.com>
2020-08-18 00:10:14 +0200

    [ArretInfoManager] Modification du manager, changements au niveau de l'api, modification des variables ( nombre de rows, sens)

--- src/Manager/ArretInfoManager.php
+++ src/Manager/ArretInfoManager.php
@@ -1,15 +1,16 @@
 <?php
+
 namespace App\Manager;
 
 use GuzzleHttp\Client;
 
 class ArretInfoManager
 {
-    public function getData(string $nomArret, int $sens)
+    public function getData(string $nomArret, int $sens, int $rowsNumber = 10)
     {
         $client = new Client();
 
-        $response = $client->get('https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-metro-circulation-passages-tr&q=&lang=fr&facet=nomcourtligne&facet=sens&facet=destination&facet=nomarret&facet=precision&refine.nomarret=' . $nomArret . '&refine.sens=' . $sens);
+        $response = $client->get('https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-metro-circulation-passages-tr&q=&rows=' . $rowsNumber . '&lang=fr&facet=nomcourtligne&facet=sens&facet=destination&facet=nomarret&facet=precision&refine.precision=Temps+réel&refine.nomarret=' . $nomArret . '&refine.sens=' . $sens);
 
         return json_decode($response->getBody());
     }
