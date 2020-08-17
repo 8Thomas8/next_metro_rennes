f7102e97309e6aa96c91b4a7caf93c0285b8605a
Thomas ROUSSEAU <thomas.rousseau@protonmail.com>
2020-08-18 00:11:37 +0200

    Modification du controller et du template de la vue pour afficher les DateTime au bon format, ainsi que le temps restant avant le départ.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,10 +5,13 @@ namespace App\Controller;
 use App\Entity\Arret;
 use App\Forms\ArretForm;
 use App\Manager\ArretInfoManager;
+use DateTimeZone;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use DateTime;
+use Symfony\Component\Validator\Constraints\Timezone;
 use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -33,6 +36,7 @@ class HomeController extends AbstractController
      * @throws LoaderError
      * @throws RuntimeError
      * @throws SyntaxError
+     * @throws \Exception
      */
     public function index(Request $request): Response
     {
@@ -41,27 +45,24 @@ class HomeController extends AbstractController
         $formArret = $this->createForm(ArretForm::class, $search);
         $formArret->handleRequest($request);
 
-        $result0 = [];
-        $result1 = [];
         $dataSens0 = [];
         $dataSens1 = [];
-        $nextArret0 = new Arret();
-        $nextArret0->setNomArret("");
-        $nextArret0->setDepart("");
-        $nextArret0->setDestination("");
-        $nextArret0->setError("Aucune donnée pour le moment.");
+        $nextDepart0 = new Arret();
+        $nextDepart0->setNomArret("");
+        $nextDepart0->setDepart(new DateTime());
+        $nextDepart0->setDestination("");
+        $nextDepart0->setError("Aucune donnée pour le moment.");
 
-        $nextArret1 = new Arret();
-        $nextArret1->setNomArret("");
-        $nextArret1->setDepart("");
-        $nextArret1->setDestination("");
-        $nextArret1->setError("Aucune donnée pour le moment.");
-
+        $nextDepart1 = new Arret();
+        $nextDepart1->setNomArret("");
+        $nextDepart1->setDepart(new DateTime());
+        $nextDepart1->setDestination("");
+        $nextDepart1->setError("Aucune donnée pour le moment.");
 
         if ($formArret->isSubmitted()) {
             $arretInfoManager = new ArretInfoManager();
 
-            $result0 = array($arretInfoManager->getData($search->getNomArret(), 0))[0];
+            $result0 = array($arretInfoManager->getData($search->getNomArret(), 0, 1))[0];
             $records0 = array_column(array($result0), 'records')[0];
 
             foreach ($records0 as $e) {
@@ -70,7 +71,7 @@ class HomeController extends AbstractController
                 array_push($dataSens0, $dataArray);
             }
 
-            $result1 = array($arretInfoManager->getData($search->getNomArret(), 1))[0];
+            $result1 = array($arretInfoManager->getData($search->getNomArret(), 1, 1))[0];
             $records1 = array_column(array($result1), 'records')[0];
 
             foreach ($records1 as $e) {
@@ -80,39 +81,44 @@ class HomeController extends AbstractController
             }
 
             if (sizeof($dataSens0) != 0) {
-                $nextArret0->setNomArret($dataSens0[0]['nomarret']);
-                $nextArret0->setDestination($dataSens0[0]['destination']);
-                $nextArret0->setDepart($dataSens0[0]['depart']);
-                $nextArret0->setError('');
+                $nextDepart0->setNomArret($dataSens0[0]['nomarret']);
+                $nextDepart0->setDestination($dataSens0[0]['destination']);
+                $date0 = new DateTime($dataSens0[0]['depart']);
+                $date0->setTimezone(new DateTimeZone('Europe/Paris'));
+                $nextDepart0->setDepart($date0);
+                $nextDepart0->setError('');
             }
 
             if (sizeof($dataSens1) != 0) {
-                $nextArret1->setNomArret($dataSens1[0]['nomarret']);
-                $nextArret1->setDestination($dataSens1[0]['destination']);
-                $nextArret1->setDepart($dataSens0[0]['depart']);
-                $nextArret1->setError('');
+                $nextDepart1->setNomArret($dataSens1[0]['nomarret']);
+                $nextDepart1->setDestination($dataSens1[0]['destination']);
+                $date1 = new DateTime($dataSens0[0]['depart']);
+                $date1->setTimezone(new DateTimeZone('Europe/Paris'));
+                $nextDepart1->setDepart(new DateTime($dataSens1[0]['depart']) );
+                $nextDepart1->setError('');
 
             }
 
-            $nowDateTime = new \DateTime();
 
-//            $nextArret0->setDiff($nowDateTime->diff($nextArret0->getDepart()));
-//            $nextArret1->setDiff($nowDateTime->diff($nextArret1->getDepart()));
+            $nowDateTime = new DateTime();
+
+            $nextDepart0->setDiff(abs($nowDateTime->getTimestamp() - $nextDepart0->getDepart()->getTimestamp()));
+            $nextDepart1->setDiff(abs($nowDateTime->getTimestamp() - $nextDepart1->getDepart()->getTimestamp()));
 
         }
 
         dump($formArret);
         dump($dataSens0);
         dump($dataSens1);
-        dump($nextArret0);
-        dump($nextArret1);
+        dump($nextDepart0);
+        dump($nextDepart1);
 
         return new Response($this->twig->render('pages/home.html.twig', [
             'formArret' => $formArret->createView(),
             '$dataSens0' => $dataSens0,
             '$dataSens1' => $dataSens1,
-            'nextArret0' => $nextArret0,
-            'nextArret1' => $nextArret1
+            'nextDepart0' => $nextDepart0,
+            'nextDepart1' => $nextDepart1
         ]));
     }
 
