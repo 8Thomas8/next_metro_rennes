ec0b21173a12832ef7e59fc2183eccaf6c23e9bc
Thomas ROUSSEAU <thomas.rousseau@protonmail.com>
2020-09-10 00:38:32 +0200

    WIP: Modification inté

--- assets/js/components/content.js
+++ assets/js/components/content.js
@@ -12,8 +12,15 @@ $().ready(() => {
         }
     }
 
-    // Simule un submit quand la sélection change.
-    $('select').change(() => {
+    // Une fois le premier select modifié, passage au suivant.
+    $('.first-input').change(() => {
+            $('.first-step').hide();
+            $('.second-step').show();
+        }
+    );
+
+    // Simule un submit quand la sélection change sur la destination.
+    $('.second-step').change(() => {
             $('form').submit();
         }
     );
@@ -22,4 +29,38 @@ $().ready(() => {
     $('#edit-link').click(() => {
         window.history.back();
     });
+
+    // Decompte
+    let time = $('#time')[0].innerText;
+    let min = parseInt(time.slice(0, 2));
+    let sec =  parseInt(time.slice(3, 5));
+
+    // TODO: Besoin de la date complete
+    const countDownDate = new Date(0, 0, 0, 0, min, sec).getTime();
+
+
+    let x = setInterval(function() {
+
+        const now = new Date().getTime();
+
+        const distance = countDownDate - now;
+
+        // Calcule pour les différentes unités de temps
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        $('#time').text(minutes + ":" + seconds);
+        // If the count down is finished, write some text
+
+        // if (distance < 0) {
+        //     clearInterval(x);
+        //     $('#time').text("Trop tard");
+        // }
+
+    }, 1000);
+
+
 })
