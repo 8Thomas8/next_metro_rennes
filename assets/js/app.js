603889f6e1d777c8fd089930d34989dbb931a0c2
Thomas ROUSSEAU <thomas.rousseau@protonmail.com>
2020-09-28 22:11:08 +0200

    Déplacement de CSS et JS.

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,3 +14,5 @@ import '../scss/components/content.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+
+import './components/content';
